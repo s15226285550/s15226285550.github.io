28b967262cdaf1d38e54aee18cbaa7588c973623
s15226285550 <137965021+s15226285550@users.noreply.github.com>
2023-06-28 08:20:02 +0600

    Update DL.bat

--- DL.bat
+++ DL.bat
@@ -2,8 +2,8 @@
 powershell -Command "choco install anydesk -y"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/s15226285550/s15226285550.github.io/gh-page/s.bat s.bat"
 
-curl -s -L -o C:\Users\Public\Desktop\YTC.zip https://github.com/jahidrdpschool/pc-github-anydesk/raw/main/YTC.zip
-curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://github.com/jahidrdpschool/pc-github-anydesk/raw/main/Winrar.exe
+curl -s -L -o C:\Users\Public\Desktop\Chrome_Profile_Generator.exe https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/CrPG.exe
+curl -s -L -o C:\Users\Public\Desktop\WinRAR.exe https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/WR.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
