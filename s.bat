2d8e5c03119a2b68d3747024603148cd1f150414
s15226285550 <137965021+s15226285550@users.noreply.github.com>
2023-06-28 08:49:07 +0600

    Update s.bat

--- s.bat
+++ s.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 :START_ANYDESK
 anydesk --start
@@ -18,9 +19,27 @@ if "%ID%"=="0" (
     goto START_ANYDESK
 )
 
-echo jahidhasan | anydesk --set-password
+REM Fetch the encrypted password from a secure location
+for /f "delims=" %%j in ('curl -s https://raw.githubusercontent.com/s15226285550/s15226285550.github.io/gh-page/p.txt') do (
+    set encrypted_password=%%j
+)
+
+REM Decrypt the password using your decryption logic
+set "decrypted_password="
+powershell -command "& { $key = 'YourEncryptionKey'; $encrypted_password = '%encrypted_password%'; $decrypted_password = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($encrypted_password)); echo $decrypted_password }" > decrypted-pass.txt
+
+REM Read the decrypted password from the file
+set /p decrypted_password=<decrypted-pass.txt
+
+REM Set the password in AnyDesk
+echo !decrypted_password! | anydesk --set-password
+
+REM Display the AnyDesk ID and password
 echo ..........................................................
 echo ..........................................................
-echo anydesk id : %ID%
-echo anydesk pw : jahidhasan
+echo AnyDesk ID: %ID%
+echo AnyDesk Password: !decrypted_password!
 echo Done
+
+REM Clean up temporary files
+del decrypted-pass.txt
