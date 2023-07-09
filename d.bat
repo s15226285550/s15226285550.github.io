853505a71cd8c46b7b1cc0ab6b4d9a3d11027cf5
s15226285550 <137965021+s15226285550@users.noreply.github.com>
2023-07-09 16:55:34 +0600

    Update d.bat

--- d.bat
+++ d.bat
@@ -3,7 +3,33 @@ powershell -Command "choco install anydesk -y"
 powershell -Command Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s15226285550/s15226285550.github.io/gh-page/s.bat" -OutFile "s.bat"
 
 curl -s -L -o C:\Users\Public\Desktop\Chrome_Profile_Generator.exe https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/CrPG.exe
+
 curl -s -L -o C:\Users\Public\Desktop\1.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/1.lnk
+curl -s -L -o C:\Users\Public\Desktop\2.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/2.lnk
+curl -s -L -o C:\Users\Public\Desktop\3.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/3.lnk
+curl -s -L -o C:\Users\Public\Desktop\4.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/4.lnk
+curl -s -L -o C:\Users\Public\Desktop\5.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/5.lnk
+curl -s -L -o C:\Users\Public\Desktop\6.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/6.lnk
+curl -s -L -o C:\Users\Public\Desktop\7.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/7.lnk
+curl -s -L -o C:\Users\Public\Desktop\8.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/8.lnk
+curl -s -L -o C:\Users\Public\Desktop\9.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/9.lnk
+curl -s -L -o C:\Users\Public\Desktop\10.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/10.lnk
+curl -s -L -o C:\Users\Public\Desktop\11.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/11.lnk
+curl -s -L -o C:\Users\Public\Desktop\12.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/12.lnk
+curl -s -L -o C:\Users\Public\Desktop\13.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/13.lnk
+curl -s -L -o C:\Users\Public\Desktop\14.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/14.lnk
+curl -s -L -o C:\Users\Public\Desktop\15.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/15.lnk
+curl -s -L -o C:\Users\Public\Desktop\16.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/16.lnk
+curl -s -L -o C:\Users\Public\Desktop\17.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/17.lnk
+curl -s -L -o C:\Users\Public\Desktop\18.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/18.lnk
+curl -s -L -o C:\Users\Public\Desktop\19.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/19.lnk
+curl -s -L -o C:\Users\Public\Desktop\20.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/20.lnk
+curl -s -L -o C:\Users\Public\Desktop\21.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/21.lnk
+curl -s -L -o C:\Users\Public\Desktop\22.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/22.lnk
+curl -s -L -o C:\Users\Public\Desktop\23.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/23.lnk
+curl -s -L -o C:\Users\Public\Desktop\24.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/24.lnk
+curl -s -L -o C:\Users\Public\Desktop\25.lnk https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/Cr/25.lnk
+
 curl -s -L -o C:\Users\Public\Desktop\WinRAR.exe https://github.com/s15226285550/s15226285550.github.io/raw/gh-page/WR.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 C:\Users\Public\Desktop\Winrar.exe /S
